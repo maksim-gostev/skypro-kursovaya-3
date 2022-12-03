acb23ae8effb4b385a120c0f7af64ae9f67f9bb1
Maksim Gostev <teranys1986@gmail.com>
2022-12-03 10:21:33 +0300

    начало работы

---
+++ utils.py
@@ -0,0 +1,47 @@
+
+
+def get_posts_all():
+    """
+     возвращает посты
+    :return:
+    """
+    pass
+
+
+def get_posts_by_user(user_name):
+    """
+    возвращает посты определенного пользователя.
+    Функция должна вызывать ошибку ValueError если такого пользователя нет
+    и пустой список, если у пользователя нет постов.
+    :param user_name:
+    :return:
+    """
+    pass
+
+
+def get_comments_by_post_id(post_id):
+    """
+    возвращает комментарии определенного поста.
+    Функция должна вызывать ошибку ValueError если такого поста нет
+    и пустой список, если у поста нет комментов.
+    :param post_id:
+    :return:
+    """
+    pass
+
+def search_for_posts(query):
+    """
+    возвращает список постов по ключевому слову
+    :param query:
+    :return:
+    """
+    pass
+
+
+def get_post_by_pk(pk):
+    """
+    возвращает один пост по его идентификатору.
+    :param pk:
+    :return:
+    """
+    pass
