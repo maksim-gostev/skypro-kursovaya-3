655144f16ebb5a9ed2bd126bff1027794f6f4380
Maksim Gostev <teranys1986@gmail.com>
2022-12-04 17:41:04 +0300

    настроил коментарии к посту

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def get_all_post():
     return render_template('index.html', all_post=all_post)
 
 
-@app.route("/posts/<postid>")
-def get_posts():
+@app.route("/posts/<int:postid>")
+def get_posts(postid):
+    post_id = utils.get_post_by_pk(postid)
+    all_comments = utils.get_comments_by_post_id(postid)
+    return render_template('post.html', all_comments=all_comments, post_id=post_id, count_comment=len(all_comments))
 
 
 
--- utils.py
+++ utils.py
@@ -2,12 +2,18 @@ import json
 
 def get_posts_all():
     """
-     возвращает посты
-    :return:
+    возвращает посты
+    :return: список постов
     """
     with open('data/posts.json', 'r', encoding='utf-8') as file:
         json_file = json.load(file)
         return json_file
+
+
+def get_all_comments():
+    with open('data/comments.json', 'r', encoding='utf-8') as file:
+        json_comment_file = json.load(file)
+        return json_comment_file
 
 
 def get_posts_by_user(user_name):
@@ -22,14 +28,12 @@ def get_posts_by_user(user_name):
 
 
 def get_comments_by_post_id(post_id):
-    """
-    возвращает комментарии определенного поста.
-    Функция должна вызывать ошибку ValueError если такого поста нет
-    и пустой список, если у поста нет комментов.
-    :param post_id:
-    :return:
-    """
-    pass
+    all_comments = get_all_comments()
+    comments = []
+    for comment in all_comments:
+        if comment["post_id"] == post_id:
+            comments.append(comment)
+    return comments
 
 def search_for_posts(query):
     """
@@ -41,9 +45,11 @@ def search_for_posts(query):
 
 
 def get_post_by_pk(pk):
-    """
-    возвращает один пост по его идентификатору.
-    :param pk:
-    :return:
-    """
-    pass
+    all_post = get_posts_all()
+    post_id = []
+    for post in all_post:
+        if post["pk"] == pk:
+            post_id.append(post)
+    return post_id
+get_post_by_pk(1)
+
